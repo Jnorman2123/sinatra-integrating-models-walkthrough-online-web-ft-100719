0a011b4cd14485a14b695edfec7a14bf4001cfa6
Jonathan Norman <jnorman2123@gmail.com>
2019-11-19 20:58:27 +0000

    Automatically backed up by Learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,1 +1,28 @@
 # Your TextAnalyzer model code will go here.
+class TextAnalyzer 
+  attr_reader :text 
+
+  def initialize(text) 
+    @text = text.downcase 
+  end 
+
+  def count_of_words 
+    words = text.split(" ")
+    words.count 
+  end 
+
+  def count_of_vowels 
+    text.scan(/[aeiou]/).count
+  end 
+
+  def count_of_consonants 
+    text.scan(/[bcdfghjklmnpqrstvwxyz]/).count 
+  end 
+
+  def most_used_letters 
+    s1 = text.gsub(/[^a-z]/, '')
+    arr = s1.split('')
+    arr1 = arr.uniq 
+    arr2 = {}
+  end 
+end 
